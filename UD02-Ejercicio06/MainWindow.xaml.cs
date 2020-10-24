232f83a00b5509e7baa5ae3d575872568c0c9dd6
Francisco Julian Gil Marin <fj.gil16@iesdoctorbalmis.com>
2020-10-24 13:58:47 +0200

    Agregar archivos de proyecto.

---
+++ UD02-Ejercicio06/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UD02_Ejercicio06
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpacidadImagen_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 1;
+        }
+
+        private void OpacidadImagen_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Image).Opacity = 0.5;
+        }
+    }
+}
